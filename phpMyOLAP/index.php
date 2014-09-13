0dab8e15de085f366a2f968e32d7c76bb7b77aa6
Ap.Muthu <apmuthu@usa.net>
2014-09-13 21:50:42 +0530

    Replaced index.html with index.php

---
+++ phpMyOLAP/index.php
@@ -0,0 +1,25 @@
+<?php
+include("config.php");
+?>
+<html>
+
+<head>
+<link rel="stylesheet" type="text/css" href="style/style.css" />
+</head>
+
+<body>
+
+<h2>phpMyOLAP Login</h2>
+<form action="home.php" method="POST">
+Username: <input type=text name=username><br><br>
+Password: <input type=password name=password></br><br>
+&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; 
+<input type=submit value=ok>
+</form>
+
+
+</body>
+
+</html>
+
+
