e23f65903707babf921abb620892081df0fe2803
Ap.Muthu <apmuthu@usa.net>
2014-09-13 22:42:34 +0530

    Icon AltText String Translation to English and Social Site Urls standardisation

--- phpMyOLAP/olap/share_fb.php
+++ phpMyOLAP/olap/share_fb.php
@@ -14,27 +14,29 @@ $shareurl="$urlsito/olap/report.php?parametri=$cubename*$levels*$colonna*$ordina
 print "<div style='margin-top:10px'>";
 print "<center>";
 
-//facebook
-print "<script src='http://static.ak.fbcdn.net/connect.php/js/FB.Share' type='text/javascript'></script>";
-print "<a icon_url='$img_report' name='fb_share' type='button_count' href='http://www.facebook.com/sharer.php?u=$shareurl' target='blank'>";
-print "<img src='$img_fb' width=40px height=40px alt='condividi su facebook'/></a>";
+// facebook
+//print "<script src='http://static.ak.fbcdn.net/connect.php/js/FB.Share' type='text/javascript'></script>";
+// print "<a icon_url='$img_report' name='fb_share' type='button_count' href='http://www.facebook.com/sharer.php?u=$shareurl' target='blank'>";
+print "<a href='#' onclick=\"window.open('http://www.facebook.com/sharer.php?u=$shareurl');return false;\" title=\"Share on Facebook\" target=\"_blank\">";
+print "<img src='$img_fb' width=40px height=40px alt='share on facebook'/></a>";
 
-//twittwee
-print "<script type='text/javascript' src='http://platform.twitter.com/widgets.js'></script>";
-print "<a href='#' onclick=\"window.open('http://twitter.com/share?url=$shareurl');return false;\" title=\"Condividi su Twitter\" target=\"_blank\">";
-print "<img src='$img_tw' width=40px height=40px alt='condividi su facebook'/></a>";
+// twitter
+//print "<script type='text/javascript' src='http://platform.twitter.com/widgets.js'></script>";
+print "<a href='#' onclick=\"window.open('http://twitter.com/share?url=$shareurl');return false;\" title=\"Share on Twitter\" target=\"_blank\">";
+print "<img src='$img_tw' width=40px height=40px alt='share on Twitter'/></a>";
 
 
-//google
-print "<a href='https://m.google.com/app/plus/x/?v=compose&content=$shareurl' onclick=\"window.open('https://m.google.com/app/plus/x/?v=compose&content=$shareurl')\",\"gplusshare\";\"return false;\" title=\"Condividi su Google Plus!\" target=\"_blank\">";
-print "<img src='$img_google' width=40px height=40px alt='condividi su googleplus'>";
+// google plus
+// print "<a href='https://m.google.com/app/plus/x/?v=compose&content=$shareurl' onclick=\"window.open('https://m.google.com/app/plus/x/?v=compose&content=$shareurl')\",\"gplusshare\";\"return false;\" title=\"Share on Google Plus!\" target=\"_blank\">";
+print "<a href='#' onclick=\"window.open('https://m.google.com/app/plus/x/?v=compose&content=$shareurl');return false;\" title=\"Share on Google Plus\" target=\"_blank\">";
+print "<img src='$img_google' width=40px height=40px alt='share on Google Plus'>";
 print "</a>";
 
-//linkedin
+// linkedin
 //print "<script type='text/javascript' src='http://platform.linkedin.com/in.js'></script>";
 //print "<script type='in/share' data-url='$shareurl' data-counter='top'></script>";
-print "<a href='http://www.linkedin.com/shareArticle?mini=true&amp;url=$shareurl&amp;title=phpMyOLAP'  title='Condividi su LinkedIn' target=\"_blank\">";
-print "<img src='$img_linkedin' width=40px height=40px alt='condividi su linkedin'>";
+print "<a href='http://www.linkedin.com/shareArticle?mini=true&amp;url=$shareurl&amp;title=phpMyOLAP'  title='Share on LinkedIn' target=\"_blank\">";
+print "<img src='$img_linkedin' width=40px height=40px alt='share on LinkedIn'>";
 print "</a>";
 
 
