a7b29b8aec46238275ee23a1196943c20ea6deae
Ap.Muthu <apmuthu@usa.net>
2014-09-13 22:44:03 +0530

    Updated fpdf library from v1.5.1 to v1.7.1

--- phpMyOLAP/fpdf/tutorial/tuto1.php
+++ phpMyOLAP/fpdf/tutorial/tuto1.php
@@ -1,9 +1,7 @@
 <?php
-define('FPDF_FONTPATH','../font/');
 require('../fpdf.php');
 
-$pdf=new FPDF();
-$pdf->Open();
+$pdf = new FPDF();
 $pdf->AddPage();
 $pdf->SetFont('Arial','B',16);
 $pdf->Cell(40,10,'Hello World!');
